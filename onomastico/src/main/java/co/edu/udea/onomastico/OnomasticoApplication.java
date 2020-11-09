f82661551934d1eeb9ff4f8a291d388bed39fe46
jenny <hidej26@gmail.com>
2020-11-09 18:13:07 -0500

    initial commit

---
+++ onomastico/src/main/java/co/edu/udea/onomastico/OnomasticoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.onomastico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnomasticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnomasticoApplication.class, args);
+	}
+
+}
---
+++ onomastico/src/test/java/co/edu/udea/onomastico/OnomasticoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.onomastico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnomasticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
