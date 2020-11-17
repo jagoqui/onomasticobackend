9501131a7c8d1dc01c61ff4d2bbf349c0ca4def5
Andres Vasquez <apoyoitingenieria@udea.edu.co>
2020-11-17 15:14:24 -0500

    Update OnomasticoApplicationTests.java

--- src/test/java/co/edu/udea/onomastico/OnomasticoApplicationTests.java
+++ src/test/java/co/edu/udea/onomastico/OnomasticoApplicationTests.java
@@ -3,11 +3,11 @@ package co.edu.udea.onomastico;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OnomasticoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
