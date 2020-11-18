ffce446a7ff4136331f46d9f1278444af6f71d81
jenny.escobar <apoyodesarroyoingenieria6@udea.edu.co>
2020-11-18 10:53:41 -0500

    crossorigin configuration

--- src/main/java/co/edu/udea/onomastico/controller/UsuarioCorreoController.java
+++ src/main/java/co/edu/udea/onomastico/controller/UsuarioCorreoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,8 @@ import co.edu.udea.onomastico.model.UsuarioCorreo;
 import co.edu.udea.onomastico.model.UsuarioCorreoId;
 import co.edu.udea.onomastico.repository.UsuarioCorreoRepository;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 public class UsuarioCorreoController {
 
