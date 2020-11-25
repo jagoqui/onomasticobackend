a63b23b92c40092ea9555b283d20d969253aa85a
jenny.escobar <apoyodesarroyoingenieria6@udea.edu.co>
2020-11-25 18:40:46 -0500

    se agrega informacion al token

--- src/main/java/co/edu/udea/onomastico/security/JwtTokenProvider.java
+++ src/main/java/co/edu/udea/onomastico/security/JwtTokenProvider.java
@@ -28,7 +28,10 @@ public class JwtTokenProvider {
 		Date now = new Date();
 		Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
 
-		return Jwts.builder().setSubject(Long.toString(userPrincipal.getId())).claim("rol", userPrincipal.getAuthorities().toString()).setIssuedAt(new Date())
+		return Jwts.builder().setSubject(Long.toString(userPrincipal.getId()))
+				.claim("rol", userPrincipal.getAuthorities().iterator().next().toString())
+				.claim("nombre", userPrincipal.getUsername())
+				.setIssuedAt(new Date())
 				.setExpiration(expiryDate).signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
 	}
 
